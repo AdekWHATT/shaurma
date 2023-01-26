9651fe88bbc77281b2fb1f38888d75419f038bcc
Vyacheslav K <vyacheslaw.kilin@mail.ru>
2023-01-26 11:45:27 +0300

    решить ошибку dotenv

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -16,15 +16,15 @@ import {
   where,
   addDoc,
 } from "firebase/firestore";
-
+require('dotenv').config()
 const firebaseConfig = {
-  apiKey: "AIzaSyBZkm1qqmcFot6tEci4i4abU61f9HYDWKQ",
-  authDomain: "shaurma-42214.firebaseapp.com",
-  projectId: "shaurma-42214",
-  storageBucket: "shaurma-42214.appspot.com",
-  messagingSenderId: "1085867038549",
-  appId: "1:1085867038549:web:e611ecc9728ab60c5241dc",
-  measurementId: "G-Z8LNTWKZ7L"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
